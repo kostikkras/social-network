b60f1a0ef4e54149ff5f7b4afc70f19549dd0943
Konstantin <pikula.konstantin@yandex.ru>
2023-02-14 16:56:57 +0300

    add Loader

---
+++ src/widgets/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import './Loader.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className }: LoaderProps) => {
+
+    return <div className={classNames('lds-roller', {}, [className])} >
+                <div>
+                </div>
+                <div>
+                </div>
+                <div>
+                </div>
+                <div>
+                </div>
+            </div>
+};
