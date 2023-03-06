39cb92f217bbf1cf4a167847a96d5021107e6905
Konstantin <konstantn.pikula@yandex.ru>
2023-03-06 22:14:01 +0300

    fix loginSlice

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,5 +1,5 @@
 import { LoginModal } from './ui/LoginModal/LoginModal';
 import { LoginSchema } from './model/types/loginSchema';
-import { loginReducer } from './model/slice/loginSlice';
+import { loginReducer } from './model/slice/LoginSlice';
 
 export { loginReducer, LoginSchema, LoginModal };
